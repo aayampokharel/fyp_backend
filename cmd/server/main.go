3eb63d40cd369fb4c6ad0e9c7aaead3b8b124a5a
aayam pokharel <aayam.pokharel@gmail.com>
2025-07-07 00:53:07 +0545

    insertion of data finalized

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,11 @@
 package main
 
+import "github.com/aayampokharel/fyp/features/block"
+
 // Middleware to log requests with colors
 
 func main() {
 	// router := chi.NewRouter()
 	// router.Get("/", func(w http.ResponseWriter, r *http.Request) {})//todo: make these more shorter , use this func signature directly for handler if possible .
+	block.CreateBlock()
 }
